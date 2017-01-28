1d23cdb67b829ce1c8b8aec71a95b59f904c7fca
Chris Underwood <cunderwoodmn@gmail.com>
2017-01-27 21:42:06 -0800

    parsed data into son

--- js/widget.js
+++ js/widget.js
@@ -3,7 +3,9 @@
 var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function () {
   if (xhr.readyState === 4) {
-    console.log(xhr.responseText);
+    var employees = JSON.parse(xhr.responseText);
+    console.log(typeof employees);
+    console.log(employees);
   }
 };
 xhr.open('GET', 'data/employees.json');
