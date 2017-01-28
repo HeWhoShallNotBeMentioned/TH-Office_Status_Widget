3a08924169574eec5be37ab149b7c6e34590d5a4
Chris Underwood <cunderwoodmn@gmail.com>
2017-01-27 22:28:31 -0800

    create UL and LI's to for employee list

--- js/widget.js
+++ js/widget.js
@@ -6,7 +6,20 @@ xhr.onreadystatechange = function () {
     var employees = JSON.parse(xhr.responseText);
     console.log(typeof employees);
     console.log(employees);
+    var statusHTML = '<ul class="bulleted">';
+    for (var i = 0; i < employees.length; i += 1) {
+      if(employees[i].inoffice === true){
+        statusHTML += '<li class="in">';
+      } else {
+        statusHTML += '<li class="out">';
+      }
+      statusHTML += employees[i].name;
+      statusHTML += '</li>';
+    }
+    statusHTML += '</ul>';
+      console.log(statusHTML);
   }
+
 };
 xhr.open('GET', 'data/employees.json');
 xhr.send();
